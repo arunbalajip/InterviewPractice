0c6a3e78356d49c1c59ee44dcf10412ec54eebb8
arunbalaji Prithiviraj <arunbalaji.java000@gmail.com>
2021-04-07 21:42:36 -0700

    Update editDistance.java

--- editDistance.java
+++ editDistance.java
@@ -1,4 +1,21 @@
-  public static int editDistance(String source, String target) {
+/*
+        Suppose we have  a string x of length n and a string y of length m, 
+        and we want to find the edit distance between x and y. 
+        
+        we here have a function editDistance(a,b) that gives the edit distance between prefixes x[0...a] and y[0...b].
+         Thus, using this function, the edit distance between x and y equals editDistance(n−1,m−1).
+          We can calculate values of distance as follows: 
+                        editDistance(a,b)=min(editDistance(a,b−1)+1, editDistance(a−1,b)+1, editDistance(a−1,b−1)+cost(a,b)) 
+                        
+                        Here cost(a,b) = 0 if x[a] = y[b], and otherwise cost(a,b) = 2(remove old charcter and add new one). 
+                        
+          The formula considers the following ways to edit the string x: 
+                • distance(a,b−1): insert a character at the end of x 
+                • distance(a−1,b): remove the last character from x 
+                • distance(a−1,b−1): match or modify the last character of x            
+        */
+  
+public static int editDistance(String source, String target) {
         int n1=source.length(), n2=target.length();
         int ans = Integer.MAX_VALUE ;
         char[] sourceArray = source.toCharArray();
